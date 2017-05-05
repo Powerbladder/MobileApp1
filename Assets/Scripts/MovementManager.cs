2071d2152900811b4d92dc7b3a608aa3ce06a79c
Powerbladder <mebake@gmail.com>
2017-05-05 07:30:37 -0700

    Probably have some movement code in there that i will remove
    Wanted to pass along some very nice news shared during our meeting at Engage with Gary Zorko of ZGal.  Gary was the only rep from ZG and Noel, Bill, Barb Pranke, Terry Esposito and I attended from Aptos.
    
    Gary just raved about the way you guys delivered the project.  Said the schedule was tight, the delivery was of high quality, the team worked well together and that the users are very excited as well.  He was overall very happy and it helped remove some of the sting around the overall project delay.
    
    I wanted to pass that along to you and thank you for all your hard work on this.  The results are showing very clearly.
    
    Pls pass this along to anyone I might have missed.  My apologies if I did.

---
+++ Assets/Scripts/MovementManager.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class MovementManager : MonoBehaviour {
+
+    public float moveTime = 0.1f;
+    public LayerMask blockingLayer; // Collision detection
+
+    private BoxCollider bc;
+    private Rigidbody rb;
+    private float inverseMoveTime;
+
+    // This class handles all of the movement for the game
+
+
+	// Use this for initialization
+	protected virtual void Start()
+    {
+        bc = GetComponent<BoxCollider>();
+        rb = GetComponent<Rigidbody>();
+        inverseMoveTime = 1f / moveTime;
+	}
+
+    protected bool Move(int xDir, int zDir, out RaycastHit2D hit)
+    {
+        Vector2 start = transform.position;
+        Vector2 end = start + new Vector2(xDir, zDir);
+
+        bc.enabled = false;
+        hit = Physics2D.Linecast(start, end, blockingLayer);
+        bc.enabled = true;
+        
+        if(hit.transform == null)
+        {
+            StartCoroutine(SmoothMovement(end));
+            return true;
+        }
+
+        return false;
+    }
+
+    protected IEnumerator SmoothMovement (Vector3 end)
+    {
+        float sqrRemainingDistance = (transform.position - end).sqrMagnitude;
+
+        while(sqrRemainingDistance > float.Epsilon)
+        {
+            Vector3 newPosition = Vector3.MoveTowards(rb.position, end, inverseMoveTime * Time.deltaTime);
+            rb.MovePosition(newPosition);
+            sqrRemainingDistance = (transform.position - end).sqrMagnitude;
+            yield return null;
+        }
+    }
+
+    protected virtual void AttemptMove<T>(int xDir, int yDir)
+        where T : Component
+    {
+        RaycastHit2D hit;
+        bool canMove = Move(xDir, yDir, out hit);
+
+        if(hit.transform == null)
+            return;
+
+        T hitComponent = hit.transform.GetComponent<T>();
+
+        if (!canMove && hitComponent != null)
+            OnCantMove(hitComponent);
+        
+    }
+
+    protected abstract void OnCantMove<T>(T component)
+        where T : Component;
+}
